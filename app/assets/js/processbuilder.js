261966e650e5f579a371c12892ad03be0f77b0cc
toster234 <minecraft.thomas234@gmail.com>
2023-03-08 13:20:50 +0100

    MSG

--- app/assets/js/processbuilder.js
+++ app/assets/js/processbuilder.js
@@ -504,7 +504,7 @@ class ProcessBuilder {
                             val = args[i].replace(argDiscovery, tempNativePath)
                             break
                         case 'launcher_name':
-                            val = args[i].replace(argDiscovery, 'Helios-Launcher')
+                            val = args[i].replace(argDiscovery, 'TostHost-Launcher')
                             break
                         case 'launcher_version':
                             val = args[i].replace(argDiscovery, this.launcherVersion)
